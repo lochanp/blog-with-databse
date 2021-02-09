b948d696de8602bd48a6bc5d8c4f8abb750e69cd
Lochan Potdar <lochan.potdar1998@gmail.com>
2021-02-09 19:45:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "This is a blog post website here you can create new blogs and updload it on the site to for the users to read it. Anyone can create a blog on any topic for other users to read";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+   
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+  
+  Post.findOne({_id:requestedPostId},(err,post) => {
+    res.render('post',{
+      title:post.title,
+      content:post.content
+    })
+  });
+})
+
+
+app.get("/about", function(req, res){
+  res.render("about")
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
